39b0101943b64ddc608d73cc7ac064b828ea8643
LAPL29 <perazalugo1@gmail.com>
2023-10-16 20:20:57 -0700

    nuevo
    que rollo pa

---
+++ build/js/app.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+
+    for (let i = 1; i < 13; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+    
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg"
+        alt="Imagen Galeria">
+        `;
+
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+
+    }
+
+
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+
+    <source srcset="build/img/grande/${id}.avif" type="image/avif">
+    <source srcset="build/img/grande/${id}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg"
+    alt="Imagen Galeria">
+    `;
+
+    //Crea el overlay con la imgen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove(); // para que lo elimine
+    }
+
+    
+
+    //boton para cerrar el modal
+
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove(); // para que lo elimine
+    }
+
+    overlay.appendChild(cerrarModal);
+
+    // añadirlo al html
+    const body = document.querySelector('body');
+    body.appendChild(overlay); // overlay para que salga mas oscuro al hacer click
+    body.classList.add('fijar-body');
+}
+
